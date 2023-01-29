54e161f23da813f74c8f7c50dc9b4c92288606b6
Noah Hicks <noahg@ad.unc.edu>
2023-01-29 13:05:05 -0500

    sends api request

--- cli.js
+++ cli.js
@@ -46,3 +46,8 @@ let timezone = moment.tz.guess();
 if('-t' in args){
     timezone  =  args['-t'];
 }
+
+//Send API request 
+let req_url = "https://api.open-meteo.com/v1/forecast?latitude=" + latitude + "&longitude=" + longitude + "&daily=precipitation_hours&current_weather=true&timezone=" + timezone;
+const response = await fetch(req_url);
+let data = response.json();
