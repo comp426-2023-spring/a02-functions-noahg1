74ecd3433fb047c1f563fb204f6365ef80b01651
Noah Hicks <noahg@ad.unc.edu>
2023-01-29 12:29:47 -0500

    Help text and timezone added

---
+++ cli.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+// Usage: galosh.js [options] -[n|s] LATITUDE -[e|w] LONGITUDE -z TIME_ZONE
+//     -h            Show this help message and exit.
+//     -n, -s        Latitude: N positive; S negative.
+//     -e, -w        Longitude: E positive; W negative.
+//     -z            Time zone: uses tz.guess() from moment-timezone by default.
+//     -d 0-6        Day to retrieve weather: 0 is today; defaults to 1.
+//     -j            Echo pretty JSON from open-meteo API and exit.
+
+import { Moment } from "moment-timezone"
+
+const timezone = moment.tz.guess()
