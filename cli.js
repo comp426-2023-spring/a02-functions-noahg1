6ff95b6e4d16b8f49aba9bb92ee88407bd3cd2df
Noah Hicks <noahg@ad.unc.edu>
2023-01-29 12:53:12 -0500

    Help text added

--- cli.js
+++ cli.js
@@ -1,28 +1,23 @@
 #!/usr/bin/env node
-// Usage: galosh.js [options] -[n|s] LATITUDE -[e|w] LONGITUDE -z TIME_ZONE
-//     -h            Show this help message and exit.
-//     -n, -s        Latitude: N positive; S negative.
-//     -e, -w        Longitude: E positive; W negative.
-//     -z            Time zone: uses tz.guess() from moment-timezone by default.
-//     -d 0-6        Day to retrieve weather: 0 is today; defaults to 1.
-//     -j            Echo pretty JSON from open-meteo API and exit.
 
-import { Moment } from "moment-timezone"
+import moment from "moment-timezone";
+import minimist from "minimist";
+import fetch from "node-fetch";
 
-const timezone = moment.tz.guess()
+const args = minimist(process.argv.slice(2))
 
-// Make a request
-const response = await fetch('https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m');
-
-// Get the data from the request
-const data = await response.json();
-
-const days = args.d 
-
-if (days == 0) {
-  console.log("today.")
-} else if (days > 1) {
-  console.log("in " + days + " days.")
-} else {
-  console.log("tomorrow.")
+if('-h' in args){
+    console.log(`
+        Usage: galosh.js [options] -[n|s] LATITUDE -[e|w] LONGITUDE -z TIME_ZONE
+            -h            Show this help message and exit.
+            -n, -s        Latitude: N positive; S negative.
+            -e, -w        Longitude: E positive; W negative.
+            -z            Time zone: uses tz.guess() from moment-timezone by default.
+            -d 0-6        Day to retrieve weather: 0 is today; defaults to 1.
+            -j            Echo pretty JSON from open-meteo API and exit.`);
+    process.exit(0);
 }
+
+
+
+
